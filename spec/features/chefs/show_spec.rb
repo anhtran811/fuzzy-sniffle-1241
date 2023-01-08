098204f21e685ba7bfbf4c9c343414658c0be1a1
Anhnhi Tran <anhtran811@gmail.com>
2023-01-08 15:08:21 -0700

    Started extension, ran out of time

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -91,14 +91,14 @@ RSpec.describe 'Chef show page' do
     end
   end
 
+  # As a visitor
+  # When I visit a chef's show page
+  # I see a link to view a list of all ingredients that this chef uses in their dishes.
+  # When I click on that link
+  # I'm taken to a chef's ingredients index page
+  # and I can see a unique list of names of all the ingredients that this chef uses.
+  
   describe 'User story 3' do
-# As a visitor
-# When I visit a chef's show page
-# I see a link to view a list of all ingredients that this chef uses in their dishes.
-# When I click on that link
-# I'm taken to a chef's ingredients index page
-# and I can see a unique list of names of all the ingredients that this chef uses.
-
     it 'has a link to all the ingredients this chef uses in their dishes' do
       tom = Chef.create!(name: 'Tom') 
 
@@ -133,6 +133,39 @@ RSpec.describe 'Chef show page' do
         click_link('All ingredients')
         expect(current_path).to eq("/chefs/#{tom.id}/ingredients")
       end
+    end
+  end
+
+  # As a visitor
+  # When I visit a chef's show page
+  # I see the three most popular ingredients that the chef uses in their dishes
+  # (Popularity is based off of how many dishes use that ingredient)
+  describe 'Extension' do
+    xit 'can show the most popular ingredients' do
+      tom = Chef.create!(name: 'Tom') 
+
+      spaghetti = tom.dishes.create!(name: 'spaghetti and meatballs', description: 'pork meatballs with red sauce')
+      lasagna = tom.dishes.create!(name: 'lasagna with meat sauce', description: 'can be made with vegetarian')
+      ziti = tom.dishes.create!(name: 'baked ziti', description: '5 different cheeses double baked')
+
+      sauce = Ingredient.create!(name: 'sauce', calories: 10)
+      pasta = Ingredient.create!(name: 'pasta', calories: 400)
+      meatball = Ingredient.create!(name: 'meatball', calories: 600)
+      cheese = Ingredient.create!(name: 'cheese', calories: 300)
+      five_cheese = Ingredient.create!(name: 'five cheese blend', calories: 250)
+      alfredo_sauce = Ingredient.create(name: 'alfredo sauce', calories: 300)
+
+      DishIngredient.create!(dish_id: spaghetti.id, ingredient_id: sauce.id)
+      DishIngredient.create!(dish_id: spaghetti.id, ingredient_id: pasta.id)
+      DishIngredient.create!(dish_id: spaghetti.id, ingredient_id: meatball.id)
+
+      DishIngredient.create!(dish_id: lasagna.id, ingredient_id: pasta.id)
+      DishIngredient.create!(dish_id: lasagna.id, ingredient_id: sauce.id)
+      DishIngredient.create!(dish_id: lasagna.id, ingredient_id: cheese.id)
+
+      DishIngredient.create!(dish_id: ziti.id, ingredient_id: pasta.id)
+      DishIngredient.create!(dish_id: ziti.id, ingredient_id: sauce.id)
+      DishIngredient.create!(dish_id: ziti.id, ingredient_id: five_cheese.id)
     end
   end
 end
