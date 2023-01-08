0ee59e0187c12696df66d628c532d1771609978a
Anhnhi Tran <anhtran811@gmail.com>
2023-01-08 13:15:34 -0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Dish.destroy_all
+Chef.destroy_all
+Ingredient.destroy_all
+DishIngredient.destroy_all
+
+tom = Chef.create!(name: 'Tom') 
+
+spaghetti = tom.dishes.create!(name: 'spaghetti and meatballs', description: 'pork meatballs with red sauce')
+lasagna = tom.dishes.create!(name: 'lasagna with meat sauce', description: 'can be made with vegetarian')
+ziti = tom.dishes.create!(name: 'baked ziti', description: '5 different cheeses double baked')
+
+sauce = Ingredient.create!(name: 'sauce', calories: 10)
+pasta = Ingredient.create!(name: 'pasta', calories: 400)
+meatball = Ingredient.create!(name: 'meatball', calories: 600)
+cheese = Ingredient.create!(name: 'cheese', calories: 300)
+five_cheese = Ingredient.create!(name: 'five cheese blend', calories: 250)
+
+DishIngredient.create!(dish_id: spaghetti.id, ingredient_id: sauce.id)
+DishIngredient.create!(dish_id: spaghetti.id, ingredient_id: pasta.id)
+DishIngredient.create!(dish_id: spaghetti.id, ingredient_id: meatball.id)
+
+DishIngredient.create!(dish_id: lasagna.id, ingredient_id: pasta.id)
+DishIngredient.create!(dish_id: lasagna.id, ingredient_id: sauce.id)
+DishIngredient.create!(dish_id: lasagna.id, ingredient_id: cheese.id)
+
+DishIngredient.create!(dish_id: ziti.id, ingredient_id: pasta.id)
+DishIngredient.create!(dish_id: ziti.id, ingredient_id: sauce.id)
+DishIngredient.create!(dish_id: ziti.id, ingredient_id: five_cheese.id)
